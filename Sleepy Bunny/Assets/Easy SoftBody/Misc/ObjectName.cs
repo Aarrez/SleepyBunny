20b332d4795478f0a12d009bfb01477acc1f7f70
Theo Hogben <theo@tyrantfilms.no>
2022-10-24 14:10:20 +0100

    Change to soft body phyics and bed

---
+++ Sleepy Bunny/Assets/Easy SoftBody/Misc/ObjectName.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ObjectName : MonoBehaviour {
+	public spawnSBObject _spawner;
+	public string[] _Names;
+	private Text _thisText;
+	// Use this for initialization
+	void Start () {
+		_thisText = GetComponent<Text> ();
+		_thisText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_thisText.text = _Names [_spawner._queue];
+	}
+
+	public void EnableText(){
+	
+		_thisText.enabled = true;
+	
+	}
+}
